b885e79775278abecf264486c3929feaf8eced9b
sagar joshi <sagarjo637@gmail.com>
2026-06-02 08:40:38 +0545

    pratice java basics

---
+++ basics/Variable.java
@@ -0,0 +1,9 @@
+class Variable{
+    public static void main(String[] args){
+        String name="Sagar Joshi";
+        int semester=2;
+        System.out.println(name);
+        System.out.println(semester);
+
+    }
+}
