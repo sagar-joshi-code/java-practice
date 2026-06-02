88edef82cb06b1c7fe67de068adaf27503db9c8f
sagar joshi <sagarjo637@gmail.com>
2026-06-02 08:49:48 +0545

    practiced datatype in java

---
+++ Datatype.java
@@ -0,0 +1,15 @@
+//practiced program using all datatype
+class datatype{
+    public static void main(String[] args){
+        String name="Sagar joshi";
+        boolean passed=true;
+        char grade='A';
+        double marks=85.5;
+        int age=20;
+        System.out.println(name);
+        System.out.println(passed);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(age);
+    }
+}
