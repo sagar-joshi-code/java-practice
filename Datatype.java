fee90049bb83d3ddb670141d9af1c4943b4543ec
sagar joshi <sagarjo637@gmail.com>
2026-06-08 15:49:58 +0545

    Practice how to take input from the user and also practice command line argument

--- Datatype.java
+++ Datatype.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 //practiced program using all datatype
 class datatype{
     public static void main(String[] args){
@@ -31,5 +32,72 @@ class student{
         System.out.println("Age:" + age);
         System.out.println("Semester:" + semester);
         System.out.println("Grade:" + grade);
+    }
+}
+
+// class Hello{
+//     public static void main(String[] args){
+//         int age=20;
+//         String name="Sagar";
+//         System.out.printf("Name=%s\n",name);
+//         System.out.printf("Age=%d\n",age);
+//     }
+// }
+
+//command line argument
+class CommandLine{
+    public static void main(String[] args){
+        System.out.println(args[0]);
+         System.out.println(args[1]);
+        
+    }
+}
+
+//Taking Integer Input Using Scanner
+class InputDemo{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        System.out.println("Number= " + n);
+    }
+}
+
+//Taking String Input Using Scanner
+class NameInput{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter name: ");
+        String name=sc.nextLine();
+        System.out.println("Name = " + name);
+    }
+}
+//takinf string and integer both
+class Both{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name=sc.nextLine();
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+// Simple Student Information System
+
+class StudentInfo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name=sc.nextLine();
+        System.out.print("Enter Roll: ");
+        int roll=sc.nextInt();
+
+        System.out.println("\n----- Student Information -----");
+        System.out.println("Name: " + name);
+        System.out.println("Roll no: " + roll);
     }
 }
