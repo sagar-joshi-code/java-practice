3c7b66dd82dc28fb53871255bf9d4e4dad9e54aa
sagar joshi <sagarjo637@gmail.com>
2026-06-08 22:42:04 +0545

    practice swapping two value and practice arithmetic operation

--- variable and operator/Variable.java
+++ variable and operator/Variable.java
@@ -16,3 +16,49 @@ class Addition{
 
         }
 }
+
+
+
+
+//Arithmetic Operations
+
+class Arithmetic{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //taking input from the user
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+
+        //performing calculation
+        System.out.println("Addition = " + (a+b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Division = " + (a/b));
+        System.out.println("Modulus = " + (a%b));
+
+    }
+}
+
+//Swap two number
+class Swap{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        //taking two number from the user
+        System.out.print("Enter  value of a: ");
+        int a=sc.nextInt();
+        System.out.print("Enter value of b: ");
+        int b=sc.nextInt();
+
+        //swapping values
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+
+    }
+}
