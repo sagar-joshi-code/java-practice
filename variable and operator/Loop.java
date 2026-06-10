14547684f5f49ef34c657f353cb3293db53dbd60
sagar joshi <sagarjo637@gmail.com>
2026-06-10 08:19:40 +0545

    practice loop question

--- variable and operator/Loop.java
+++ variable and operator/Loop.java
@@ -22,3 +22,21 @@ class Print{
 
     }
 }
+
+//sum of first N number
+class SumN{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        int sum=0;
+
+        for(int i=0;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("sum is " + sum);
+
+
+
+    }
+}
